84e00f3d209f5cd420d5664835b2e6f4beb0a16e
Richard-bit120 <arzarichard@gmail.com>
2026-09-07 11:31:30 -0300

    feat(friendship): add routes, controller, service, repository

--- src/modules/friendship/friendship.routes.ts
+++ src/modules/friendship/friendship.routes.ts
@@ -1,9 +1,11 @@
-import { Router } from 'express';
-import { respondFriendship } from './friendship.controller';
-import { authenticateJWT } from '../../middleware/auth.middleware';
+import { Router } from "express";
+import { respondFriendship } from "./friendship.controller";
+import { authenticateJWT } from "../../middleware/auth.middleware";
 const router = Router();
 
 // PATCH /friendships/:id/respond
-router.patch('/:id/respond', authenticateJWT, respondFriendship);
+router.patch("/:id/respond", authenticateJWT, respondFriendship);
+
+//router.patch("/:id/respond", respondFriendship);
 
 export default router;
