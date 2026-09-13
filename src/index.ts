abe209a2cd52251de5ce97549f2aa22b46ae4c1e
martin-varela4 <martinchovarela2@gmail.com>
2026-09-13 20:13:21 -0300

    Cambios necesarios para poder conectar los jugadores  al frontend y acceder a filtrado de busqueda por posición

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,34 @@ dotenv.config();
 
 import "reflect-metadata";
 import express from "express";
+import cors from 'cors'; 
 import reviewRoutes from "./modules/review/review.routes";
 import userRoutes from "./modules/user/user.routes";
 import { AppDataSource } from "./database/data-source";
 
-
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:5173", 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use("/api/reviews", reviewRoutes); 
 app.use("/api/users", userRoutes);
 
-app.use(express.json());
 
 AppDataSource.initialize()
     .then(() => {
         console.log("Base de datos conectada");
-        app.listen(process.env.PORT || 8080, () => {
-            console.log(`Servidor en puerto ${process.env.PORT || 8080}`);
+        const PORT = process.env.PORT || 8080;
+        app.listen(PORT, () => {
+            console.log(`Servidor corriendo en el puerto ${PORT}`);
         });
     })
     .catch((err: Error) => {
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,7 +3,7 @@ import * as userService from "./user.service"
 
 export const searchPlayers = async (req: Request, res: Response) => {
   try {
-    const term = req.query.q as string | undefined;
+    const term = req.query.nombre as string | undefined;
 
     const players = await userService.searchPlayers(term);
     res.status(200).json(players);
